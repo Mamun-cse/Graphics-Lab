8ca56e8ba06ceb7d9df8d882ee32369e2f41694e
Mamun-cse <37404399+Mamun-cse@users.noreply.github.com>
2020-02-26 12:24:34 +0600

    Random Line

--- RandomLine.cpp
+++ RandomLine.cpp
@@ -1,39 +1,24 @@
-#include<Windows.h>
-#define MX 99999
+#include<bits/stdc++.h>
 #include<graphics.h>
-#include<time.h>
-#include <unistd.h>
+using namespace std;
 
 int main()
 {
     int gd=DETECT,gm;
     initgraph(&gd,&gm,"");
+    int n=0;
 
-    for(int t=1; t<=20; t++)
-    {
-        setcolor(GREEN);
-        line(rand()%450,rand()%450,rand()%450,rand()%450);
-        usleep(MX);
-        cleardevice();
+    while(1){
+        n++;
+        setcolor(rand()%20);
+        line(rand()%200,rand()%300,rand()%200,rand()%400);
+        delay(100);
 
-        setcolor(RED);
-        line(rand()%350,rand()%350,rand()%350,rand()%350);
-        usleep(MX);
-        cleardevice();
+        if(n>100){
+            cleardevice();
+            n=0;
+        }
 
-        setcolor(BLUE);
-        line(rand()%450,rand()%350,rand()%450,rand()%350);
-        usleep(MX);
-        cleardevice();
-
-        setcolor(GREEN);
-        line(rand()%450,rand()%450,rand()%450,rand()%450);
-        setcolor(RED);
-        line(rand()%350,rand()%350,rand()%350,rand()%350);
-        setcolor(BLUE);
-        line(rand()%450,rand()%350,rand()%450,rand()%350);
-        usleep(MX);
-        cleardevice();
     }
     getch();
     closegraph();
